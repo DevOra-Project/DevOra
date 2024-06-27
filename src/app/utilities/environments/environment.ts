b650fd1e5e7027313fb69b06b4de498d6f0b8b80
Mufment <jorleotuppiac@gmail.com>
2024-06-27 13:57:37 -0500

    "Update API URL in environment files"

--- src/app/utilities/environments/environment.ts
+++ src/app/utilities/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080'
+    apiUrl: 'https://devoraecho-production.up.railway.app'
+    //apiUrl: 'http://localhost:8080'
   };
